e37e5e275dead22660ed7311c9d23125d21f940f
Chad.Sowald <chad.sowald@rightside.co>
2016-03-29 12:33:46 -0700

    Added a test that reads all lines.

---
+++ AgidIronyParser.Tests/AgidTests.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Linq;
+using Irony.Parsing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AgidIronyParser.Tests
+{
+    [TestClass]
+    public class AgidTests
+    {
+        [TestMethod]
+        public void ReadAgid()
+        {
+            var grammar = new InflGrammer();
+            var numLines = 0;
+
+            foreach (var root in File.ReadLines("infl.txt").Select(line => GetRoot(line, grammar)))
+            {
+                numLines++;
+                ValidateNodes(root);
+            }
+
+            Assert.AreEqual(112505, numLines);
+        }
+
+        /// <summary>
+        /// Recursively validate nodes.
+        /// </summary>
+        private static void ValidateNodes(ParseTreeNode node)
+        {
+            Assert.IsNotNull(node);
+            Assert.IsFalse(node.IsError);
+
+            foreach (var child in node.ChildNodes)
+            {
+                ValidateNodes(child);
+            }
+        }
+
+        private static ParseTreeNode GetRoot(string sourceCode, Grammar grammar)
+        {
+            var language = new LanguageData(grammar);
+            var parser = new Parser(language);
+            var parseTree = parser.Parse(sourceCode);
+            return parseTree.Root;
+        }
+    }
+}
